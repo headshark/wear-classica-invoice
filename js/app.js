5ee7c44aeac8e621e3bc8011ef14ee9f866de0a7
Patrick <torralbapatrick15@gmail.com>
2020-01-17 14:46:35 -0600

    Add multi step form

--- js/app.js
+++ js/app.js
@@ -7,4 +7,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
 showTab = (n) => {
     let tab = document.querySelectorAll('.tab');
     tab[n].style.display = 'block';
+    
+    if (n === 0) {
+        document.getElementById('prevBtn').style.display = 'none';
+    } else {
+        document.getElementById('prevBtn').style.display = 'inline';
+    }
+
+    if (n == (tab.length - 1)) {
+        document.getElementById('nextBtn').innerHTML = 'Submit';
+    } else {
+        document.getElementById('nextBtn').innerHTML = 'Next';
+    }
+}
+
+nextPrev = (n) => {
+    let tab = document.querySelectorAll('.tab');
+    
+    tab[currentTab].style.display = "none";
+    currentTab = currentTab + n;
+    
+    showTab(currentTab);
 }
