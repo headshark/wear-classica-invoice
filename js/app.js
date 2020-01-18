83a662c96382c76c144c75d23f95510449efca77
Patrick <torralbapatrick15@gmail.com>
2020-01-18 17:55:28 -0600

    Add form validation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let currentTab = 0;
-let totalAmount = 0;
+let customer = [];
 let items = [];
 const provinces = [
     'Abra', 'Agusan del Norte', 'Aklan', 'Albay', 'Antique', 'Apayao', 'Aurora',
@@ -39,6 +39,7 @@ let tab = document.querySelectorAll('.tab');
 document.addEventListener('DOMContentLoaded', (event) => {
     populateProvinceDatalist();
     showTab(currentTab);
+    customer.totalAmount = 0;
 });
 nextBtn.addEventListener('click', () => {
     nextPrev(1);
@@ -78,11 +79,63 @@ showTab = (n) => {
     }
 }
 
-nextPrev = (n) => { 
+nextPrev = (n) => {
+    if (n == 1 && !validateForm()) return false;
+
     tab[currentTab].style.display = "none";
     currentTab = currentTab + n;
+    console.log(customer);
     
     showTab(currentTab);
+}
+
+validateForm = () => {
+    let valid = true;
+    
+    if (currentTab === 0 || currentTab == 2) {
+        let inputs = tab[currentTab].getElementsByTagName('input');
+        for (let i = 0; i < inputs.length; i++) {
+            let input = inputs[i];
+
+            if (input.value === '' && input.className === 'required') {
+                // input.className += ' invalid';
+                alert(input.name + ' is a required field.');
+                valid = false;
+            } else {
+                customer[input.name] = input.value;
+            }
+        }
+    }
+
+    if (currentTab === 1) {
+        if (items.length === 0) {
+            alert('please add an item.');
+            valid = false;
+        }
+    }
+
+    if (currentTab === 3) {
+        let payments = document.getElementsByName('payment');
+        for (let i = 0; i < payments.length; i++) {
+            let payment = payments[i];
+
+            if (payment.checked) {
+                customer.paymentMethod = payment.value;
+                valid = true;
+                test = true;
+
+                break;
+            } else {
+                valid = false;
+            }
+        }
+
+        if (!valid) {
+            alert('please select a payment method');
+        }
+    }
+    
+    return valid;
 }
 
 addItem = () => {
@@ -90,25 +143,31 @@ addItem = () => {
     let itemTable = document.getElementById('itemTable');
     let itemDescTxt = document.getElementById('itemDescTxt');
     let itemAmountTxt = document.getElementById('itemAmountTxt');
-    let row = itemTable.insertRow(itemTable.rows.length);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    
-    updateTotalAmount(itemAmountTxt.value);
-    cell1.innerHTML = itemDescTxt.value;
-    cell2.innerHTML = itemAmountTxt.value;
-    item.description = itemDescTxt.value;
-    item.amount = itemAmountTxt.value;
-    items.push(item);
 
-    // reset input
-    itemDescTxt.value = '';
-    itemAmountTxt.value = '';
+    if (itemDescTxt.value !== '' && itemAmountTxt.value !== '') {
+        let row = itemTable.insertRow(itemTable.rows.length);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        
+        updateTotalAmount(itemAmountTxt.value);
+        cell1.innerHTML = itemDescTxt.value;
+        cell2.innerHTML = itemAmountTxt.value;
+        item.description = itemDescTxt.value;
+        item.amount = itemAmountTxt.value;
+        items.push(item);
+        customer.items = items;
+    
+        // reset input
+        itemDescTxt.value = '';
+        itemAmountTxt.value = '';
+    } else {
+        alert('please enter the item description and amount.');
+    }
 }
 
 updateTotalAmount = (n) => {
-    totalAmount += parseFloat(n);
-    document.getElementById('totalAmount').innerHTML = totalAmount;
+    custtotalAmount += parseFloat(n);
+    document.getElementById('totalAmount').innerHTML = custtotalAmount;
 }
 
 populateInvoiceDetails = () => {
